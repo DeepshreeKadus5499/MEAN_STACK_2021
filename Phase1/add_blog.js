f8b918f3ae7aed5db44e89f75652735eb05c8787
DeepshreeKadus5499 <kadusdeepshree5499@gmail.com>
2021-07-31 21:08:24 +0530

    final commit

--- Phase1/add_blog.js
+++ Phase1/add_blog.js
@@ -1,6 +1,4 @@
 function addBlog(){
-  
-
     var title = document.getElementById("title").value;
     var article = document.getElementById("article").value;
 
@@ -11,17 +9,13 @@ function addBlog(){
     var titletext = document.createTextNode(""+title);
     var ptagcontent2 = document.createTextNode(""+article);
 
-
     // var image2= document.getElementById("output")
     // var img1 = document.createElement('img');
     // img1.src = "blog1.png"
     // img1.width=100,
     // img1.height=100
 
-
     divtag.setAttribute("class","card")
-
- 
     h5.appendChild(titletext)
     ptag1.appendChild(ptagcontent2)
  
@@ -41,7 +35,3 @@ function reset() {
     document.getElementById("file").value=""; 
 }
 
-function deleteInfo(count) {
-    console.log("delete fun called.."+count);
-    document.getElementById(count).remove();
-}
