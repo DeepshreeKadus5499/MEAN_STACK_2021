c2a5581873314318f8ee49e0a40fe5bd92252b8f
DeepshreeKadus5499 <kadusdeepshree5499@gmail.com>
2022-02-22 00:33:41 +0530

    added mongo cloud

--- OnlineGroceryStore/server/config/config.js
+++ OnlineGroceryStore/server/config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        connectionString: 'mongodb://localhost:27017/GroceryStore'
+        // connectionString: 'mongodb://localhost:27017/GroceryStore'
+        connectionString: 'mongodb+srv://deepshree:deepshree5499@groceryapp.6zqxi.mongodb.net/GroceryStore?retryWrites=true&w=majority'
     },
     production: {
 
